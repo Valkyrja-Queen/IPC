ee44f50ca2c0b5d8fd01f932e016c226fe12c660
Valkyrja <valkyrja45queen@outlook.jp>
2024-06-20 06:15:49 +0900

    cargo

---
+++ testpack/target/debug/deps/testpack.d
@@ -0,0 +1,5 @@
+D:\Study\Linux\IPC\testpack\target\debug\deps\testpack.exe: src/main.rs
+
+D:\Study\Linux\IPC\testpack\target\debug\deps\testpack.d: src/main.rs
+
+src/main.rs:
---
+++ testpack/target/debug/testpack.d
@@ -0,0 +1,1 @@
+D:\Study\Linux\IPC\testpack\target\debug\testpack.exe: D:\Study\Linux\IPC\testpack\src\main.rs
